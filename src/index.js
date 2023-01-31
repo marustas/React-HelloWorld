c309c08dd22268688597ee1eb77928eeab9c97d1
Stanislau Marudau <marudov.stas@gmail.com>
2023-01-31 21:35:40 +0200

    Demo app cration started

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import MyComponent from './MyComponent';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
+import { robots } from './robot';
+import 'tachyons'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-        root.render( <React.StrictMode >
-          <MyComponent greeting1 ={'Hello, ' + 'ReactBaby'} greeting2 ={'and ' + 'ReactNinja'}/>
-            <App/>
+        root.render( 
+        <React.StrictMode >
+            <div>
+            <Card id ={robots[0].id} name={robots[0].name} email ={robots[0].email}/>
+            <Card id ={robots[1].id} name={robots[1].name} email ={robots[1].email}/>
+            <Card id ={robots[2].id} name={robots[2].name} email ={robots[2].email}/>
+            </div>
             </React.StrictMode>
         );
 
