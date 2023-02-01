a66e1742c91c5543ff6539051421415ffeeaa7de
Stanislau Marudau <marudov.stas@gmail.com>
2023-02-01 21:24:13 +0200

    Search robot feature added

--- src/App.js
+++ src/App.js
@@ -3,16 +3,31 @@ import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import { robots } from "./robot";
 
+class App extends React.Component { 
+    //Making a STATE instead of PROPS
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
 
-const App = () => { 
+onSearchChange = (event) => {
+    this.setState({searchfield: event.target.value})
+}
+    render () {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+                })
         return(
             <div className='tc'>
                 <h1>RoboFriends</h1>
-                <SearchBox/>
-                <CardList robots ={robots}/>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots ={filteredRobots}/>
             </div>
-            );
-
+        );
+    }
 }
 
 export default App;
