df480e6a73323400947b29232c12e56264542902
Stanislau Marudau <marudov.stas@gmail.com>
2023-02-01 20:54:46 +0200

    Styling added

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import CardList from "./CardList";
+import SearchBox from "./SearchBox";
+import { robots } from "./robot";
+
+
+const App = () => { 
+        return(
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox/>
+                <CardList robots ={robots}/>
+            </div>
+            );
+
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import CardList from './CardList'
-import { robots } from './robot';
+import App from './App'
 import reportWebVitals from './reportWebVitals';
 import 'tachyons'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( 
-    <React.StrictMode >
-    <CardList robots ={robots}/>
-    </React.StrictMode>
-);
+root.render(<App />);
 
 reportWebVitals();
