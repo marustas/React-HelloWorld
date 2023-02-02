51d1e86a78cefa706eda75d1e6cb978973b130d4
Stanislau Marudau <marudov.stas@gmail.com>
2023-02-02 22:31:09 +0200

    Styles added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import { robots } from "./robot";
+import "./App.css";
 
 class App extends React.Component { 
     //Making a STATE instead of PROPS
@@ -22,7 +23,7 @@ onSearchChange = (event) => {
                 })
         return(
             <div className='tc'>
-                <h1>RoboFriends</h1>
+                <h1 className="f1">RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <CardList robots ={filteredRobots}/>
             </div>
