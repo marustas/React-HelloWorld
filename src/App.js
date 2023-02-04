0b9e7207a3b0a2349338783b0316074567b50644
Stanislau Marudau <marudov.stas@gmail.com>
2023-02-04 11:58:18 +0200

    API added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robot";
 import "./App.css";
 
+// class sytax for SMART components with STATE
 class App extends React.Component { 
     //Making a STATE instead of PROPS
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
-
+componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=>response.json())
+    .then(users=>this.setState({robots: users }))
+}
 onSearchChange = (event) => {
     this.setState({searchfield: event.target.value})
 }
@@ -21,13 +25,18 @@ onSearchChange = (event) => {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
                 })
-        return(
-            <div className='tc'>
-                <h1 className="f1">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots ={filteredRobots}/>
-            </div>
-        );
+                if(this.state.robots.length === 0){
+                    return <h1>Loading</h1>
+                }else{
+                    return(
+                        <div className='tc'>
+                            <h1 className="f1">RoboFriends</h1>
+                            <SearchBox searchChange={this.onSearchChange}/>
+                            <CardList robots ={filteredRobots}/>
+                        </div>
+                    );
+                }
+
     }
 }
 
