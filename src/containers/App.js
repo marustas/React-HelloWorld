9ba6a3fc5eeaea09b1d11a5b310fca928fc25a59
Stanislau Marudau <marudov.stas@gmail.com>
2023-02-07 23:03:43 +0200

    File system optimized

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import CardList from "./CardList";
-import Scroll from "./Scroll";
-import SearchBox from "./SearchBox";
+import CardList from "../components/CardList";
+import Scroll from "../components/Scroll";
+import SearchBox from "../components/SearchBox";
 import "./App.css";
 
 // class sytax for SMART components with STATE
@@ -23,13 +23,13 @@ onSearchChange = (event) => {
     this.setState({searchfield: event.target.value})
 }
     render () {
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        const {robots, searchfield} = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
                 })
-                if(this.state.robots.length === 0){
-                    return <h1>Loading</h1>
-                }else{
-                    return(
+                return !robots.length ? 
+                <h1>Loading</h1> :
+                (
                         <div className='tc'>
                             <h1 className="f1">RoboFriends</h1>
                             <SearchBox searchChange={this.onSearchChange}/>
@@ -37,9 +37,7 @@ onSearchChange = (event) => {
                             <CardList robots ={filteredRobots}/>
                             </Scroll>
                         </div>
-                    );
-                }
-
+                );
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App'
+import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render( <App/> );
 
 reportWebVitals();
