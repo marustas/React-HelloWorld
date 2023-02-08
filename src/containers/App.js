f81ff35531871c07ec25852f583afecfae2b6d8a
Stanislau Marudau <marudov.stas@gmail.com>
2023-02-08 21:28:51 +0200

    Search by email added

--- src/containers/App.js
+++ src/containers/App.js
@@ -26,7 +26,8 @@ onSearchChange = (event) => {
     render () {
         const {robots, searchfield} = this.state;
         const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase()) ||
+            robot.email.toLowerCase().includes(searchfield.toLowerCase())
                 })
                 return !robots.length ? 
                 <h1>Loading</h1> :
