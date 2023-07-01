fa7d0d60a275031bc5c4acf5c38aa8a7d9589770
Stanislau Marudau <marudov.stas@gmail.com>
2023-07-01 15:21:38 +0300

    Gender change implemented

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import './Counter.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSkull } from '@fortawesome/free-solid-svg-icons';
+import { faSkull, faMars, faVenus } from '@fortawesome/free-solid-svg-icons';
 
 const Counter = () => {
     const [level, setLevel] = useState(1);
     const [gear, setGear] = useState(0);
-
+    const [gender, setGender] = useState('male');
     const handleLevelIncrement = () => {
         if (level < 10) {
             setLevel(level + 1);
@@ -31,32 +31,42 @@ const Counter = () => {
         setGear(0);
     };
 
+    const handleToggleGender = () => {
+        setGender(gender === 'male' ? 'female' : 'male');
+    };
+
     const power = level + gear;
 
     return (
         <div>
-                <div className='Inline'>
-                    <div className='mr4'>
+            <div className='Inline'>
+                <div className='mr4'>
                     <h3>Power</h3>
                     <h1>{power}</h1>
-                    </div>
-                    <button className="Death" onClick={handleResetGear}>
-                        <FontAwesomeIcon className='pa0 h2' icon={faSkull} />
-                    </button>
                 </div>
+                <button className="Death" onClick={handleResetGear}>
+                    <FontAwesomeIcon className='pa0 h2' icon={faSkull} />
+                </button>
+            </div>
             <div>
+                <button className='Gender' onClick={handleToggleGender}>
+                    {gender === 'male' ? (
+                        <FontAwesomeIcon className='h2' icon={faMars} />
+                    ) : (
+                        <FontAwesomeIcon className='h2' icon={faVenus} />
+                    )}
+                </button>
             </div>
             <div className='BottomStats'>
                 <div className='mr4'>
                     <h3>Level</h3>
                     <div>
-                    <button className={level === 10 ? 'arrow-up-limit' : 'arrow-up'} onClick={handleLevelIncrement}></button>
+                        <button className={level === 10 ? 'arrow-up-limit' : 'arrow-up'} onClick={handleLevelIncrement}></button>
                         <h1>{level}</h1>
                         <button className={level === 1 ? 'arrow-down-limit' : 'arrow-down'} onClick={handleLevelDecrement}></button>
-
                     </div>
                 </div>
-                <div className='di ml4'>
+                <div className='ml4'>
                     <h3>Gear</h3>
                     <button className='arrow-up' onClick={handleGearIncrement}></button>
                     <h1>{gear}</h1>
