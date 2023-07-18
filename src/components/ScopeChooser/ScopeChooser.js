a4fac848af57d0442e8a867d1bed7f32463c3247
Stanislau Marudau <marudov.stas@gmail.com>
2023-07-18 16:50:14 +0300

    Better formatting style acquired

--- src/components/ScopeChooser/ScopeChooser.js
+++ src/components/ScopeChooser/ScopeChooser.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
 import "./ScopeChooser.css";
 const ScopeChooser = ({ onScopeChange }) => {
-    return (
-        <div>
-            <button className='BtnDoor' onClick={() => onScopeChange('doors')}></button>
-            <button className='BtnTreasure' onClick={() => onScopeChange('treasures')}></button>
-        </div>
-    );
-}
+  return (
+    <div>
+      <button
+        className="BtnDoor"
+        onClick={() => onScopeChange("doors")}
+      ></button>
+      <button
+        className="BtnTreasure"
+        onClick={() => onScopeChange("treasures")}
+      ></button>
+    </div>
+  );
+};
 export default ScopeChooser;
--- src/containers/App.js
+++ src/containers/App.js
@@ -10,61 +10,60 @@ import { treasures } from "./CardSet/treasures";
 import { doors } from "./CardSet/doors";
 
 class App extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            treasures: treasures,
-            doors: doors,
-            searchfield: '',
-            scope: ''
-        }
-    }
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value })
-    }
-    onScopeChange = (scope) => {
-        this.setState({ scope: scope })
-    }
-    render() {
-        const { treasures, searchfield, scope, doors } = this.state;
-        const filteredTreasures = treasures.filter(treasure => {
-            return treasure.id.toLowerCase().includes(searchfield.toLowerCase())
-        })
-        const filteredDoors = doors.filter(door => {
-            return door.id.toLowerCase().includes(searchfield.toLowerCase())
-        })
+  constructor() {
+    super();
+    this.state = {
+      treasures: treasures,
+      doors: doors,
+      searchfield: "",
+      scope: "",
+    };
+  }
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+  onScopeChange = (scope) => {
+    this.setState({ scope: scope });
+  };
+  render() {
+    const { treasures, searchfield, scope, doors } = this.state;
+    const filteredTreasures = treasures.filter((treasure) => {
+      return treasure.id.toLowerCase().includes(searchfield.toLowerCase());
+    });
+    const filteredDoors = doors.filter((door) => {
+      return door.id.toLowerCase().includes(searchfield.toLowerCase());
+    });
 
-        return !treasures.length ?
-            <h1>Loading</h1> :
-            (
-                <div className='tc'>
-                    <h1 className="f1">MunchkinCards </h1>
-                    <SearchBox searchChange={this.onSearchChange} />
-                    <Counter />
-                    <ScopeChooser onScopeChange={this.onScopeChange} />
-                    {scope === 'doors' ?
-                        <div>
-                            <Scroll>
-                                <ErrorBoundary>
-                                    <CardList scope={scope} cards={filteredDoors} />
-                                </ErrorBoundary>
-                            </Scroll>
-                        </div>
-                        : (scope === 'treasures' ?
-                            <Scroll>
-                                <ErrorBoundary>
-                                    <CardList scope={scope} cards={filteredTreasures} />
-                                </ErrorBoundary>
-                            </Scroll>
-                            : <Scroll>
-                                <ErrorBoundary>
-                                </ErrorBoundary>
-                            </Scroll>
-                        )
-                    }
-                </div>
-            );
-    }
+    return !treasures.length ? (
+      <h1>Loading</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">MunchkinCards </h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Counter />
+        <ScopeChooser onScopeChange={this.onScopeChange} />
+        {scope === "doors" ? (
+          <div>
+            <Scroll>
+              <ErrorBoundary>
+                <CardList scope={scope} cards={filteredDoors} />
+              </ErrorBoundary>
+            </Scroll>
+          </div>
+        ) : scope === "treasures" ? (
+          <Scroll>
+            <ErrorBoundary>
+              <CardList scope={scope} cards={filteredTreasures} />
+            </ErrorBoundary>
+          </Scroll>
+        ) : (
+          <Scroll>
+            <ErrorBoundary></ErrorBoundary>
+          </Scroll>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
